cd8382392eef80143fae12c21077bb8bf269dd38
Ezz10099 <playpassezz@gmail.com>
2026-05-21 19:43:28 +0300

    Switch game canvas to mobile portrait

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import { BattleScene } from './scenes/BattleScene.js';
 const config = {
   type: Phaser.AUTO,
   parent: 'game',
-  backgroundColor: '#121018',
+  backgroundColor: '#0c0911',
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
-    width: 960,
-    height: 540,
+    width: 390,
+    height: 844,
   },
   scene: [BootScene, BattleScene],
 };
